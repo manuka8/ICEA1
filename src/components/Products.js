ed9b8a6f4659a4d25804558cde9106aa7f6e6da5
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-12-03 22:25:06 +0530

    Update Product.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,35 @@
-
+import { useState } from 'react';
 import '../assets/CSS/layout.css';
+export default function Product({flowerType}){
+    const [data,setData] = useState({
+        quantity:'',
+    })
 
+    const [flowers,setFlowers]=useState([]);
 
-export default function Products(){
-   
+    const handleChange=(e)=>{
+        const {name,value} = e.target;
+        setData({...data,[name]:value});
+    }
+
+    const handleQuantity=(e)=>{
+        const {quantity} = data;
+        setFlowers([...flowers,{quantity}])
+    }
     return(
-        <>
-            <div className="item1">
-                <h1>Flower Shop</h1>
-            </div>
-            <div className="item2">
-                <h4 className="card-title">Buy flowers</h4>
-                <div className="grid-container">
-                    {
-                        //product
-                    }
+        <div className="grid-item">
+
+            <div class="card">
+                <img src={require(`../assets/image/${flowerType.img}`)} />
+                <div class="card-body">
+                    <h5 class="card-title">{flowerType.name} Price: {flowerType.price}</h5>
+                    <div class="quantity-container">
+                        <label for="quantity">Quantity:</label>
+                        <input type="number" id="quantity" name="quantity" onChange={handleChange} />
+                    </div>
+                    <button class="card-button" onClick={handleQuantity}>Add to Cart</button>
                 </div>
-
             </div>
-            <div className="item3">
-                {
-                //cart
-                }
-            </div>
-        </>
+        </div>
     );
-
 }
