e2030980e914f6853bab0e448e388641276621c8
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-12-03 22:55:16 +0530

    Create handleAddToCart and handleQuantityChange functions

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,18 @@
 import '../assets/CSS/layout.css';
 export default function Product(){
-    
+    const [quantity, setQuantity] = useState(0);
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            addToCart(flower, quantity);
+            setQuantity(0); 
+        }
+    };
+
+    const handleQuantityChange = (e) => {
+        const value = Math.max(0, Number(e.target.value)); 
+        setQuantity(value);
+    };
     return(
         <div className="grid-item">
 
